db8ac36ce9411a89f133d5fdc23d36e6dfcf943a
teaserfox <teaserfox@gmail>
2023-09-13 18:28:36 +0300

    '25.2'

--- app_course/tests.py
+++ app_course/tests.py
@@ -1,6 +1,5 @@
 from django.urls import reverse
 from rest_framework import status
-from rest_framework.test import APITestCase
 
 from app_course.models import Subscription
 
@@ -14,7 +13,7 @@ def test_subscribe_unsubscribe_course(self):
     }
 
     # Отправка запроса на подписку на курс
-    response = self.client.post(reverse('lms_platform:subscribe', args=[self.course.pk]), data=data)
+    response = self.client.post(reverse('app_course:subscribe', args=[self.course.pk]), data=data)
 
     self.assertEqual(response.status_code, status.HTTP_201_CREATED)  # Проверка статуса ответа
 
@@ -22,7 +21,7 @@ def test_subscribe_unsubscribe_course(self):
     self.assertEqual(Subscription.objects.filter(user=self.user, course=self.course).exists(), True)
 
     # Отправка запроса на отписку от курса
-    response = self.client.delete(reverse('lms_platform:unsubscribe', args=[self.course.pk]))
+    response = self.client.delete(reverse('app_course:unsubscribe', args=[self.course.pk]))
 
     self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)  # Проверка статуса ответа
 
